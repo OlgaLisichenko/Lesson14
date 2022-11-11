8bfdd4a471bc9ddb600291908e388c89971b4bec
Olga <olgalisichenko1@gmail.com>
2022-11-11 17:25:12 +0300

    Added changes

--- src/Task3.java
+++ src/Task3.java
@@ -5,7 +5,7 @@ public class Task3 {
     public static String stringReverse(Scanner scanner, String str) {
         System.out.println("Task 3: enter 1 to reverse " + str);
         if (scanner.nextLine().equals("1")) {
-            ToStringFunction<String> function = s -> new StringBuilder(str).reverse().toString();
+            ToStringFunction<String> function = s -> new StringBuilder(s).reverse().toString();
             return function.apply(str);
         }
         return str;
